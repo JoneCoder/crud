3ecf935917cbbba621dc8e5dcd6764b792889cdd
JoneCoder <mohummadshorifulislam@gmail.com>
2020-12-16 01:39:20 +0600

    create a controller and view. update timezone in config app.php

---
+++ codeigniter/app/Views/welcome.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codeigniter-4</title>
+</head>
+<body>
+    <h2>Welcome Ci-4!</h2>
+</body>
+</html>
